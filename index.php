bf7377a32e9d887ba9d9c40876fe7f5b24ab6c82
Syauqi Ilham <syauqiilham28@gmail.com>
2019-04-01 10:01:20 +0700

    -add table

--- index.php
+++ index.php
@@ -10,16 +10,26 @@
  	h1 { font-size: 2em; }
  	h2 { font-size: 1.75em; }
  	h3 { font-size: 1.2em; }
- 	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
+ 	table { margin-top: 0.75em; border-collapse: collapse}
+ 	th { font-size: 0.8em; text-align: left; border: none; padding-left: 0; }
  	td { padding: 0.2em; border: 0 none; }
-    input[type="text"] {
+    .table-container th { padding: 0.8em; border-bottom: 1px solid #ccc}
+    .table-container td { font-size: 1em; padding: 0.8em; border-bottom: 1px solid #ccc}
+    span {font-size: 0.8em;}
+    input[type="text"],
+    input[type="email"],
+    input[type="tel"],
+    textarea {
         padding: 8px;
         font-size: 14px;
         border: 1px solid #d7dbe8;
         border-radius: 4px;
         width: 100% !important;
         margin-bottom: 8px !important;
+    }
+    textarea{
+        resize: none;
+        height: 150px;
     }
     input[type="submit"] {
         background: #ffd200;
@@ -36,8 +46,9 @@
         text-align: center;
     }
     .container {
-        width: 400px;
-        position: absolute;
+        width: 900px;
+        height: 80%;
+        position: relative;
         background: #fff;
         border-radius: 8px;
         padding: 10px 30px 10px 30px;
@@ -46,81 +57,110 @@
         transform: translate(-50%, -50%);
         -webkit-transform: translate(-50%, -50%);
     }
+    .form-container, .table-container {
+        float: left;
+    }
+    .form-container {
+        width: 36%;
+        margin-right: 50px;
+    }
+    .table-container{ 
+        width: 58%;
+        height: 100%;
+        overflow-x: hidden;
+        overflow-y: scroll;
+    }
  </style>
  </head>
  <body>
  <div class='container'>
-    <h1>Register here!</h1>
-    <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
-    <form method="post" action="index.php" enctype="multipart/form-data" >
-        <table>
-            <tr>
-                <td>Name<br><input type="text" name="name" id="name"/></td><td>Job<br><input type="text" name="job" id="job"/></td>
-            </tr>
-            <tr>
-                <td>Email<br><input type="text" name="email" id="email"/></td><td></td>
-            </tr>
-            <tr>
-                <td colspan='2'><br><input type="submit" name="submit" value="Submit" /> &nbsp; <input type="submit" name="load_data" value="Load Data" /></td>
-            </tr>
-        </table>
-    </form>
- </div>
- <?php
-    $host = "cukildicodingserver.database.windows.net";
-    $user = "cukil";
-    $pass = "5y4uqiilh4M";
-    $db = "cukildicodingsub1db";
-    try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    } catch(Exception $e) {
-        echo "Failed: " . $e;
-    }
-    if (isset($_POST['submit'])) {
+    <div class='form-container'>
+        <h1>Register here!</h1>
+        <p>Fill in details, then click <strong>Submit</strong> to register.</p>
+        <form method="post" action="index.php" enctype="multipart/form-data" >
+            <table>
+                <tr>
+                    <td><span>Name</span><br><input type="text" name="name" id="name"/></td><td><span>Job</span><br><input type="text" name="job" id="job"/></td>
+                </tr>
+                <tr>
+                    <td><span>Email</span><br><input type="email" name="email" id="email"/></td><td><span>Phone</span><br><input type="tel" name="phone" id="phone"/></td>
+                </tr>
+                <tr>
+                    <td colspan='2'><span>Address</span><br><textarea name="address" id="address"></textarea></td>
+                </tr>
+                <tr>
+                    <td colspan='2'><br><input type="submit" name="submit" value="Submit" /></td>
+                </tr>
+            </table>
+        </form>
+    </div>
+    <div class='table-container'>
+    <?php
+        $host = "cukildicodingserver.database.windows.net";
+        $user = "cukil";
+        $pass = "5y4uqiilh4M";
+        $db = "cukildicodingsub1db";
         try {
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $job = $_POST['job'];
-            $date = date("Y-m-d");
-            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
-                        VALUES (?,?,?,?)";
-            $stmt = $conn->prepare($sql_insert);
-            $stmt->bindValue(1, $name);
-            $stmt->bindValue(2, $email);
-            $stmt->bindValue(3, $job);
-            $stmt->bindValue(4, $date);
-            $stmt->execute();
+            $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+            $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         } catch(Exception $e) {
-            echo "Failed: " . $e;
+            // echo "Failed: " . $e;
         }
-        echo "<h3>Your're registered!</h3>";
-    } else if (isset($_POST['load_data'])) {
-        try {
-            $sql_select = "SELECT * FROM Registration";
-            $stmt = $conn->query($sql_select);
-            $registrants = $stmt->fetchAll(); 
-            if(count($registrants) > 0) {
-                echo "<h2>People who are registered:</h2>";
-                echo "<table>";
-                echo "<tr><th>Name</th>";
-                echo "<th>Email</th>";
-                echo "<th>Job</th>";
-                echo "<th>Date</th></tr>";
-                foreach($registrants as $registrant) {
-                    echo "<tr><td>".$registrant['name']."</td>";
-                    echo "<td>".$registrant['email']."</td>";
-                    echo "<td>".$registrant['job']."</td>";
-                    echo "<td>".$registrant['date']."</td></tr>";
-                }
-                echo "</table>";
-            } else {
-                echo "<h3>No one is currently registered.</h3>";
+        if (isset($_POST['submit'])) {
+            try {
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $job = $_POST['job'];
+                $phone = $_POST['phone'];
+                $address = $_POST['address'];
+                $date = date("Y-m-d");
+                $sql_insert = "INSERT INTO User (name, email, job, phone, address, date) 
+                            VALUES (?,?,?,?,?,?)";
+                $stmt = $conn->prepare($sql_insert);
+                $stmt->bindValue(1, $name);
+                $stmt->bindValue(2, $email);
+                $stmt->bindValue(3, $job);
+                $stmt->bindValue(5, $phone);
+                $stmt->bindValue(6, $address);
+                $stmt->bindValue(7, $date);
+                $stmt->execute();
+            } catch(Exception $e) {
+                echo "Failed: " . $e;
             }
-        } catch(Exception $e) {
-            echo "Failed: " . $e;
-        }
-    }
- ?>
+            echo "<h3>Your're registered!</h3>";
+        } 
+            try {
+                $sql_select = "SELECT * FROM User";
+                $stmt = $conn->query($sql_select);
+                $registrants = $stmt->fetchAll(); 
+                if(count($registrants) > 0) {
+                    echo "<h2>Registered user:</h2>";
+                    echo "<table>";
+                    echo "<tr><th>Name</th>";
+                    echo "<th>Email</th>";
+                    echo "<th>Job</th>";
+                    echo "<th>Phone</th>";
+                    echo "<th>Address</th>";
+                    echo "<th>Date</th></tr>";
+                    foreach($registrants as $registrant) {
+                        echo "<tr><td>".$registrant['name']."</td>";
+                        echo "<td>".$registrant['email']."</td>";
+                        echo "<td>".$registrant['job']."</td>";
+                        echo "<td>".$registrant['phone']."</td>";
+                        echo "<td>".$registrant['address']."</td>";
+                        echo "<td>".$registrant['date']."</td></tr>";
+                    }
+                    echo "</table>";
+                } else {
+                    echo "<h3>No one is currently registered.</h3>";
+                }
+            } catch(Exception $e) {
+                echo "Failed: " . $e;
+            }
+        
+    ?>
+    </div>
+    <br style='clear: both'/>
+ </div>
  </body>
  </html>
